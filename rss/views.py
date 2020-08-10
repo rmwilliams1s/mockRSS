3c64130495699c0559f0cecd7188c1ac601df671
Rachel Williams <rwilliams1@gm.slc.edu>
2020-08-10 17:50:29 -0500

    working with forms

--- rss/views.py
+++ rss/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import User
 from .models import RSS
+from .forms import LoginForm
+
+
+# Controller for displaying login.
+def login(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == "POST":
+        form = LoginForm(request.POST, instance=user)
+        if form.is_valid():
+            return redirect('rss/feed.html', pk=user.pk)
+    else:
+        form = LoginForm(instance=user)
+    return render(request, 'rss/login.html', {'form': form})
 
 
 # Controller for displaying main page.
-def login(request):
+def feed(request):
     feeds = RSS.objects.filter(date__lte=timezone.now()).order_by('date')
     return render(request, 'rss/feed.html', {'feeds': feeds})
